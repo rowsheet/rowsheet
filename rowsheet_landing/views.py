f3c617451763071f8bc5460c51fc045529202c46
Alexander Kleinhans <alex@rowsheet.com>
2019-11-11 01:01:32 -0700

    Removing unsued models.

--- rowsheet_landing/views.py
+++ rowsheet_landing/views.py
@@ -4,8 +4,6 @@ from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe 
 
-from .models import Greeting
-
 def index(request):
     context = {
         "scripts": [
